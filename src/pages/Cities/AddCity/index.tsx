e5a1f9fbb1a54a35aea1fd2df61cbf339aaac3ae
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2021-03-14 19:08:55 -0300

    Added form to add city

--- src/pages/Cities/AddCity/index.tsx
+++ src/pages/Cities/AddCity/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { FiArrowLeft } from 'react-icons/fi';
+import { FiAlertCircle, FiArrowLeft } from 'react-icons/fi';
 
-import { Content, WizardHeader, Stepper } from './styles';
+import {
+  Content,
+  WizardHeader,
+  Stepper,
+  Step,
+  StepForm,
+  StepFormHeader,
+  StepFormSection,
+  StepFormSectionTitle,
+  StepFormSectionContent,
+  StepFormSectionField,
+  FormInput,
+  FormInputImage,
+  FormInputImagePreview,
+  FormInputTextArea,
+  StepFormFooter,
+} from './styles';
 
 interface AddCityProps {
   onAddCity(): void;
 }
 
 const AddCity: React.FC<AddCityProps> = ({ onAddCity }) => {
+  const [cityImage, setCityImage] = useState('');
+
+  function handleCityImageUpload(event: any): void {
+    setCityImage(URL.createObjectURL(event.target.files[0]));
+  }
+
   return (
     <>
       <Helmet>
@@ -22,11 +44,59 @@ const AddCity: React.FC<AddCityProps> = ({ onAddCity }) => {
         <strong>Adicionar um perfil</strong>
 
         <Stepper>
-          <span>01</span>
-          <span>02</span>
+          <Step current>01</Step>
+          <Step>02</Step>
         </Stepper>
       </WizardHeader>
-      <Content>aqui</Content>
+      <Content>
+        <StepForm>
+          <StepFormHeader>
+            <span>01</span>
+            <h1>Adicione uma cidade</h1>
+          </StepFormHeader>
+
+          <StepFormSection>
+            <StepFormSectionTitle>
+              <strong>Dados da cidade</strong>
+            </StepFormSectionTitle>
+
+            <StepFormSectionContent>
+              <StepFormSectionField>
+                <span>Nome da cidade</span>
+                <FormInput />
+              </StepFormSectionField>
+
+              <StepFormSectionField>
+                <span>Foto da cidade</span>
+
+                {cityImage ? (
+                  <FormInputImagePreview src={cityImage} />
+                ) : (
+                  <FormInputImage
+                    type="file"
+                    name="img"
+                    accept="image/*"
+                    onChange={e => handleCityImageUpload(e)}
+                  />
+                )}
+              </StepFormSectionField>
+
+              <StepFormSectionField>
+                <span>Descrição da cidade</span>
+                <FormInputTextArea />
+              </StepFormSectionField>
+            </StepFormSectionContent>
+          </StepFormSection>
+
+          <StepFormFooter>
+            <div>
+              <FiAlertCircle />
+              <p>Preencha todos os dados com cuidado</p>
+            </div>
+            <button type="button">Próximo</button>
+          </StepFormFooter>
+        </StepForm>
+      </Content>
     </>
   );
 };
--- src/pages/Cities/AddCity/styles.ts
+++ src/pages/Cities/AddCity/styles.ts
@@ -1,8 +1,14 @@
 import { shade } from 'polished';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Header from '../../../components/Header';
 
-export const Content = styled.div``;
+interface StepProps {
+  current?: boolean;
+}
+
+interface FormInputImagePreviewProps {
+  src: string;
+}
 
 export const WizardHeader = styled(Header)`
   button {
@@ -36,24 +42,217 @@ export const WizardHeader = styled(Header)`
 
 export const Stepper = styled.div`
   display: flex;
+`;
+
+export const Step = styled.span<StepProps>`
+  border-radius: 50%;
+  border: 1px solid #dce2e6;
+  color: #617480;
+  padding: 0.5rem;
+
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  ${props =>
+    props.current &&
+    css`
+      font-weight: 700;
+      background: #51b853;
+      color: #fff;
+    `}
+
+  & + span {
+    margin-left: 0.625rem;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StepForm = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 50rem;
+  height: auto;
+  margin: 3.125rem 0 3.125rem 0;
+  border-radius: 15px;
+  border: 1px solid #dce2e6;
+
+  background: #fff;
+`;
+
+export const StepFormHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 2.5rem 4rem;
+  border-radius: 15px 15px 0px 0px;
+  border-bottom: 1px solid #dce2e6;
+
+  background: linear-gradient(90deg, #f5fff5 0%, rgba(220, 245, 221, 0) 62.99%);
 
   span {
-    border-radius: 50%;
-    border: 1px solid #dce2e6;
-    color: #617480;
-    padding: 5px;
+    background: #51b853;
+    color: #fff;
+    border-radius: 10px;
+    padding: 1.3125rem;
+    margin-right: 2.5rem;
 
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 2.125rem;
+  }
+
+  h1 {
+    color: #51b853;
+    font-size: 2.25rem;
+    line-height: 2.125rem;
+  }
+`;
+
+export const StepFormSection = styled.div`
+  display: flex;
+  margin-top: 4rem;
+  padding: 0 4rem;
+
+  flex-direction: column;
+`;
+
+export const StepFormSectionTitle = styled.div`
+  padding: 1rem 0;
+
+  border-bottom: 1px solid #dce2e6;
+
+  strong {
+    font-size: 1.5rem;
+    line-height: 1.875rem;
+  }
+`;
+
+export const StepFormSectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 1.5rem;
+`;
+
+export const StepFormSectionField = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & + div {
+    margin-top: 1.5rem;
+  }
+
+  span {
+    margin-bottom: 0.625rem;
+    font-family: Heebo;
+    font-size: 0.875rem;
+    line-height: 1.375rem;
+  }
+`;
+
+export const FormInput = styled.input`
+  height: 3.5rem;
+  padding: 1.5rem;
+
+  border: 1px solid #dce2e6;
+
+  background: #f5f8fa;
+  border-radius: 10px;
+
+  line-height: 1.625rem;
+
+  color: #123952;
+`;
+
+export const FormInputImage = styled.input`
+  height: 10rem;
+  padding: 4.25rem 10rem;
+
+  border: 2px dashed #dce2e6;
+
+  background: #f5f8fa;
+  border-radius: 10px;
+
+  line-height: 1.625rem;
+
+  color: #123952;
+`;
+
+export const FormInputImagePreview = styled.div<FormInputImagePreviewProps>`
+  height: 10rem;
+  background: url(${props => props.src}) no-repeat center;
+  background-size: cover;
+
+  border-radius: 10px;
+`;
+
+export const FormInputTextArea = styled.textarea`
+  resize: none;
+
+  height: 12.5rem;
+  padding: 1.5rem;
+
+  border: 1px solid #dce2e6;
+
+  background: #f5f8fa;
+  border-radius: 10px;
+
+  line-height: 1.625rem;
+
+  color: #123952;
+`;
+
+export const StepFormFooter = styled.div`
+  display: flex;
+
+  padding: 0 4rem;
+  margin: 3.125rem 0;
+
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    display: flex;
     align-items: center;
     justify-content: center;
 
-    &:first-child {
-      border: 3px solid #dce2e6;
+    svg {
+      height: 1.625rem;
+      width: 1.625rem;
 
-      font-weight: 700;
+      color: #f25d27;
+
+      margin-right: 1.625rem;
     }
 
-    & + span {
-      margin-left: 10px;
+    p {
+      font-size: 0.875rem;
+      line-height: 1.375rem;
+      width: 8rem;
+    }
+  }
+
+  button {
+    padding: 0.75rem 2rem;
+    border: none;
+    border-radius: 10px;
+
+    background: #115d8c;
+    color: #fff;
+
+    line-height: 1.625rem;
+    font-weight: 500;
+
+    transition: background 0.5s;
+
+    &:hover {
+      background: ${shade(0.2, '#115d8c')};
     }
   }
 `;
